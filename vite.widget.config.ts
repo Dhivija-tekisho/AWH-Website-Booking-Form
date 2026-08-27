1ea3e435a4af88a8590a37d8169c007553b738a0
Dhivija-tekisho <Dhivija.Macharla@Tekisho.ai>
2026-08-28 00:38:01 +0530

    update build scripts for vercel

--- vite.widget.config.ts
+++ vite.widget.config.ts
@@ -6,7 +6,8 @@ import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 export default defineConfig({
   plugins: [react(), cssInjectedByJsPlugin()],
   build: {
-    outDir: 'dist-widget',
+    outDir: 'dist/widget',
+    emptyOutDir: false,
     lib: {
       entry: resolve(__dirname, 'src/WidgetEntry.tsx'),
       name: 'AWHChatWidget',
